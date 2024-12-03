7bb90ce8fca175f385e64538aa1074d15032e318
Pipopa <pipopa002@Pipopas-MacBook-Air.local>
2024-12-03 14:37:19 +0700

    add user routes

--- src/api/routes/user.routes.js
+++ src/api/routes/user.routes.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const UserController = require('../controllers/user.controller');
+
+router.post('/users', UserController.createUser);
+router.get('/users/email/:email', UserController.getUserByEmail);
+router.get('/users/username/:username', UserController.getUserByUsername);
+router.put('/users/:id', UserController.updateUser);
+router.delete('/users/:id', UserController.deleteUser);
+
+module.exports = router;
